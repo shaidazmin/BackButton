7e543ea8ac479881e7dc7d7141541950fd4344fe
Shaid Azmin <shaidazmin@gmail.com>
2019-05-03 17:56:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nz/backbutton/NewActivity.java
@@ -0,0 +1,25 @@
+package com.example.nz.backbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
